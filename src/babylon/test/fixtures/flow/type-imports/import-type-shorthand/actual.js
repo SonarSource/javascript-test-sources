9f94bbf5f8cd11641a920fdfce5347207ec8829e
vilchik-elena <elena.vilchik@sonarsource.com>
2018-10-01 17:13:50 +0200

    Remove flow fixtures from babel project

--- src/babylon/test/fixtures/flow/type-imports/import-type-shorthand/actual.js
+++
@@ -1,8 +0,0 @@
-import {type} from "foo";
-import {type t} from "foo";
-import {type as} from "foo";
-import {type t as u} from "foo";
-
-import {typeof t} from "foo";
-import {typeof as} from "foo";
-import {typeof t as u} from "foo";
--- src/babylon/test/fixtures/flow/type-imports/import-type/actual.js
+++
@@ -1,3 +0,0 @@
-import type Def from "M";
-import type {named} from "M";
-import type Def, {named} from "M";
