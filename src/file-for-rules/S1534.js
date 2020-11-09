8512f7b58823ab982d2f7b77d4a82c1dbd2c1659
Andrea Guarino <andrea.guarino@sonarsource.com>
2020-11-09 10:56:11 +0100

    Move file-for-rules into sonarjs repository

--- src/file-for-rules/S1534.js
+++
@@ -1,7 +0,0 @@
-var obj = {
-    key: 'value',
-    "key": 'value',
-
-    get key() { return this.key ; },
-    set key(p) { this.key = p; }
-};
--- src/file-for-rules/S5757.js
+++
@@ -1,5 +0,0 @@
-const { Signale } = require('signale');
-const options = {
-secrets: []
-};
-const logger = new Signale(options); // Sensitive
